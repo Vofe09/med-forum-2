47d7d73fea8e257456cd5db1e767fdc53c8b0731
Vofe09 <nazariikovalev@gmail.com>
2026-01-20 11:59:26 +0500

    9.5

--- pages/api/test-mail.js
+++ pages/api/test-mail.js
@@ -3,6 +3,6 @@ export const config = { runtime: "nodejs" };
 import { sendVerificationEmail } from "../../lib/mail";
 
 export default async function handler(req, res) {
-  await sendVerificationEmail("ТВОЯ_ПОЧТА@gmail.com", "123456");
+  await sendVerificationEmail("nazariikovalev@gmail.com", "123456");
   res.status(200).json({ ok: true });
 }
